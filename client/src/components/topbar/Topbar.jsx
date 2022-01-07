2df24cd9b47ccf17be04aab508613eeac595d653
Mohammed Talha <mtalha0018@gmail.com>
2022-01-07 17:41:45 +0500

    chishti printer

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbar__left">left</div>
+      <div className="topbar__right">right</div>
+    </div>
+  );
+};
+
+export default Topbar;
